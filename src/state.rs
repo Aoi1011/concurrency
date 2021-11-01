90226c888e293db3ec6f0c2192ad923b3294ab78
Aoi <ht.so.ik0102@gmail.com>
2021-11-01 21:03:39 +0700

    instruction

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,6 @@
 use borsh::{BorshSerialize, BorshDeserialize};
 
-#[derive(BorshDeserialize, BorshSerialize, Debug, Clone)]
+#[derive(BorshDeserialize, BorshSerialize, Debug, Clone, PartialEq)]
 pub struct Mail {
     pub id: String,
     pub from_address: String,
