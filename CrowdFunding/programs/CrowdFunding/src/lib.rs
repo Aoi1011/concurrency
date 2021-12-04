c9d087aec8f12623ee0bc42ceadf0f9e5f3b1ba7
Aoi <ht.so.ik0102@gmail.com>
2021-12-04 16:33:32 +0700

    modify lib.rs

--- CrowdFunding/programs/CrowdFunding/src/lib.rs
+++ CrowdFunding/programs/CrowdFunding/src/lib.rs
@@ -4,11 +4,49 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod crowd_funding {
+
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize(ctx: Context<Initialize>, _new_project_id: u64) -> ProgramResult {
+        
+        Ok(())
+    }
+
+    pub fn createProject(ctx: Context<CreateProject>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(init, payer = authority)]
+    pub state: Account<'info, State>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateProject<'info> {
+    #[account(mut)]
+    pub state: Account<'info, State>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct State {
+    authority: Pubkey,
+    project_ids: Vec<u64>,
+}
+
+// #[account]
+// pub struct Project {
+//     pub authority: Pubkey,
+//     pub project_ids: Vec<u64>,
+// }
+
+// #[derive(Clone)]
+// pub struct IProject<'info> {
+//     representative: Signer<'info>,
+// }
