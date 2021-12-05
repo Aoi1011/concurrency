df641e70bf79ce597c52e535a07aa5f60d889a61
Aoi <ht.so.ik0102@gmail.com>
2021-12-05 20:18:23 +0700

    lib.rs

--- libraries/math/src/lib.rs
+++ libraries/math/src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod functions;
+mod number;
+
+#[doc(inline)]
+pub use functions::*;
+
+#[doc(inline)]
+pub use number::*;
