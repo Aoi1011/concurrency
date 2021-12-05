a4f6dff57ab3c881251df5730de2c699d67fe20a
Aoi <ht.so.ik0102@gmail.com>
2021-12-05 09:35:47 +0700

    transfer amount in support_project

--- CrowdFunding/programs/CrowdFunding/src/lib.rs
+++ CrowdFunding/programs/CrowdFunding/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use std::collections::HashMap;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -10,12 +11,38 @@ pub mod crowd_funding {
     pub fn initialize(ctx: Context<Initialize>, _new_project: IProject) -> ProgramResult {
         let state = &mut ctx.accounts.state;
         // let copy_project_ids = old_project_ids.project_ids.clone();
-        state.projects.push(_new_project);
+        state.projects.insert(0, _new_project);
         // old_project_ids.account.project_ids
         Ok(())
     }
 
     pub fn create_project(ctx: Context<CreateProject>, _new_project: IProject) -> ProgramResult {
+        let state = &mut ctx.accounts.state;
+        let current_project = &mut state.number_of_project;
+        let next_project_id = *current_project + 1;
+
+        state.projects.insert(next_project_id, _new_project);
+        Ok(())
+    }
+
+    pub fn support_project(
+        ctx: Context<SupportProject>,
+        project_id: u64,
+        amount: u64,
+    ) -> ProgramResult {
+        let state = &mut ctx.accounts.state;
+        let all_projects = state.projects;
+        let current_amount = all_projects[&project_id].current_amount;
+
+        **ctx.accounts.authority.try_borrow_mut_lamports()? -= amount;
+        current_amount += amount;
+
+        ctx.accounts.authority.lamports.borrow();
+
+        Ok(())
+    }
+
+    pub fn delete_project(ctx: Context<CreateProject>, _new_project: IProject) -> ProgramResult {
         let state = &mut ctx.accounts.state;
         state.projects.push(_new_project);
         Ok(())
@@ -38,11 +65,19 @@ pub struct CreateProject<'info> {
     pub authority: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct SupportProject<'info> {
+    #[account(mut)]
+    pub state: Account<'info, State>,
+    pub authority: Signer<'info>,
+}
+
 #[account]
 #[derive(Default)]
 pub struct State {
     pub authority: Pubkey,
-    pub projects: Vec<IProject>,
+    pub number_of_project: u64,
+    pub projects: HashMap<u64, IProject>,
 }
 
 // #[account]
@@ -55,6 +90,8 @@ pub struct State {
 pub struct IProject {
     project_id: u64,
     representative: Pubkey,
+    current_amount: u64,
+    goal_amount: u64,
     deadline: u16,
     achieved: bool,
 }
