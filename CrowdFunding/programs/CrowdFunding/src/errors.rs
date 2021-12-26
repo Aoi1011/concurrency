3147a2aba34b7dd8a6bd717c345c4c40ec2bb8df
Aoi <ht.so.ik0102@gmail.com>
2021-12-26 11:35:38 +0700

    errors

--- CrowdFunding/programs/CrowdFunding/src/errors.rs
+++ CrowdFunding/programs/CrowdFunding/src/errors.rs
@@ -1,11 +1,13 @@
-// use anchor_lang::error;
+use anchor_lang::error;
 // use solana_program::program_error::ProgramError;
 
-// #[error]
-// pub enum ErrorCode {
-//     #[msg("CrowdFunding is over")]
-//     CrowdFundingOver,
-// }
+// pub type 
+
+#[error]
+pub enum ErrorCode {
+    #[msg("CrowdFunding is over")]
+    CrowdFundingOver,
+}
 
 // impl From<math::Error> for ErrorCode {
 //     fn from(_: math::Error) -> ErrorCode {
