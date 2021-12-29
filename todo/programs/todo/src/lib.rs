49196ce27a9c682b74aab9045bb0a0a625cfc7fc
Aoi <ht.so.ik0102@gmail.com>
2021-12-29 22:47:59 +0700

    todolist

--- todo/programs/todo/src/lib.rs
+++ todo/programs/todo/src/lib.rs
@@ -12,3 +12,12 @@ pub mod todo {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct TodoList {
+    pub list_owner: Pubkey,
+    pub bump: u8,
+    pub capacity: u16,
+    pub name: String,
+    pub lines: Vec<Pubkey>,
+}
