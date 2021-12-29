baf5c9ff9d70bc72da424dddccc43342f5b8ab79
Aoi <ht.so.ik0102@gmail.com>
2021-12-29 23:24:23 +0700

    create newlist function

--- todo/programs/todo/src/lib.rs
+++ todo/programs/todo/src/lib.rs
@@ -5,7 +5,21 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod todo {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    // pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    //     Ok(())
+    // }
+    pub fn new_list(
+        ctx: Context<NewList>,
+        name: String,
+        capacity: u16,
+        account_bump: u8,
+    ) -> ProgramResult {
+        // create a new account
+        let list = &mut ctx.accounts.list;
+        list.list_owner = *ctx.accounts.user.to_account_info().key;
+        list.bump = account_bump;
+        list.name = name;
+        list.capacity = capacity;
         Ok(())
     }
 }
@@ -20,4 +34,29 @@ pub struct TodoList {
     pub capacity: u16,
     pub name: String,
     pub lines: Vec<Pubkey>,
+}
+
+impl TodoList {
+    fn space(name: &str, capacity: u16) -> usize {
+        // discriminator + owner pubkey + bump + capacity
+        8 + 32 + 1 + 2 + 4 + name.len() + 4 + (capacity as usize) * std::mem::size_of::<Pubkey>()
+    }
+}
+
+fn name_seed(name: &str) -> &[u8] {
+    let b = name.as_bytes();
+    if b.len() > 32 {
+        &b[0..32]
+    } else {
+        b
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(name: String, capacity: u16, list_bump: u8)]
+pub struct NewList<'info> {
+    #[account(init, payer = user, space = TodoList::space(&name, capacity), seeds=[b"todolist", user.to_account_info().key.as_ref(), name_seed(&name)], bump=list_bump)]
+    pub list: Account<'info, TodoList>,
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
