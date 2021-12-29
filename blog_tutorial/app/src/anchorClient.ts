5cac280d77d532e57299d9eb1d4642174d19768d
Aoi <ht.so.ik0102@gmail.com>
2021-12-29 20:32:13 +0700

    edit anchorClient

--- blog_tutorial/app/src/anchorClient.ts
+++ blog_tutorial/app/src/anchorClient.ts
@@ -1,41 +1,55 @@
-declare global {
-    interface Window {
-        solana: any;
-    }
-}
 import * as anchor from "@project-serum/anchor";
 import { Connection, PublicKey } from "@solana/web3.js";
 import idl from "./idl/blog_tutorial.json";
-
 import { WalletAdapterPhantom } from "./helpers/wallet-adapter-phantom";
 import { Program } from "@project-serum/anchor";
+
+import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
+import {
+  useWallet,
+  WalletProvider,
+  ConnectionProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletConnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+declare global {
+  interface Window {
+    solana: any;
+  }
+}
 
 const { SystemProgram, Keypair } = anchor.web3;
 const opts = {
   preflightCommitment: "processed",
 };
 
-const wallet =
-  window.solana.isConnected && window.solana?.isPhantom
-    ? new WalletAdapterPhantom()
-    : keypair
-    ? new anchor.Wallet(keypair)
-    : new anchor.Wallet(anchor.web3.Keypair.generate());
+// const wallet =
+//   window.solana.isConnected && window.solana?.isPhantom
+//     ? new WalletAdapterPhantom()
+//     : keypair
+//     ? new anchor.Wallet(keypair)
+//     : new anchor.Wallet(anchor.web3.Keypair.generate());
+const wallets = [getPhantomWallet()];
 
-const provider = new anchor.Provider(connection, wallet, opts);
+// const provider = new anchor.Provider(connection, wallet, opts);
 // const program = new anchor.Program(idl, programId, provider);
 const programId = new PublicKey(idl.metadata.address);
 
-export default class AnchorClient {
-  constructor({ programId, config, keypair } = {}) {
-    this.programId = programId;
-    this.config = config;
-    this.connection = new anchor.web3.Connection(
-      this.config.httpUir,
-      "confirmed"
-    );
-  }
-}
+// export default class AnchorClient {
+//   constructor({ programId, config, keypair } = {}) {
+//     this.programId = programId;
+//     this.config = config;
+//     this.connection = new anchor.web3.Connection(
+//       this.config.httpUir,
+//       "confirmed"
+//     );
+//   }
+// }\
+const wallet = useWallet();
 
 async function getProvider() {
   const network = "https://api.devnet.solana.com";
@@ -44,13 +58,16 @@ async function getProvider() {
   const provider = new anchor.Provider(
     connection,
     wallet,
-    "confirmed"
+    // @ts-ignore
+    opts.preflightCommitment
   );
   return provider;
 }
 
 async function initialize() {
   const blogAccount = anchor.web3.Keypair.generate();
+  const provider = await getProvider();
+  // @ts-ignore
   const program = new Program(idl, programId, provider);
   await program.rpc.initialize({
     accounts: {},
