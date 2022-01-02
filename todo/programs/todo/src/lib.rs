fd4f4a12b7dab9138363c83531e11387113d7ada
Aoi <ht.so.ik0102@gmail.com>
2022-01-02 16:37:52 +0700

    finsh Accounts

--- todo/programs/todo/src/lib.rs
+++ todo/programs/todo/src/lib.rs
@@ -159,12 +159,34 @@ pub struct Add<'info> {
 #[derive(Accounts)]
 #[instruction(list_name: String)]
 pub struct Cancel<'info> {
-    #[account(mut, has_one=list_owner @ TodoListError::WrongListOwner, seeds=[b"todolist", list_owner.to_account_info().key.as_ref(), name_seed(&list_name)], bump=list.bump)]
+    #[account(
+        mut, 
+        has_one=list_owner @ TodoListError::WrongListOwner, 
+        seeds=[b"todolist", list_owner.to_account_info().key.as_ref(), name_seed(&list_name)], 
+        bump=list.bump
+    )]
     pub list: Account<'info, TodoList>,
     pub list_owner: AccountInfo<'info>,
     #[account(mut)]
     pub item: Account<'info, ListItem>,
     #[account(mut, address=item.creattor @ TodoListError::WrongItemCreator)]
     pub item_creator: AccountInfo<'info>,
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(list_name: String)]
+pub struct Finish<'info> {
+    #[account(
+        mut, 
+        has_one=list_owner @ TodoListError::WrongListOwner, 
+        seeds=[b"todolist", list_owner.to_account_info().key.as_ref(), name_seed(&list_name)], 
+        bump=list.bump
+    )]
+    pub list: Account<'info, TodoList>,
+    #[account(mut)]
+    pub list_owner: AccountInfo<'info>,
+    #[account(mut)]
+    pub item: Account<'info, ListItem>,
     pub user: Signer<'info>,
 }
