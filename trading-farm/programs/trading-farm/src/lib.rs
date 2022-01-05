e5bf5574c1e9c1a67091e54f3efacc7e0cbfa25d
Aoi <ht.so.ik0102@gmail.com>
2022-01-05 07:43:45 +0700

    cancel_offer.rs handler function

--- trading-farm/programs/trading-farm/src/lib.rs
+++ trading-farm/programs/trading-farm/src/lib.rs
@@ -3,15 +3,29 @@ use anchor_lang::prelude::*;
 pub mod instructions;
 pub mod state;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+use instructions::*;
+
+declare_id!("FomuV9eM7xe3yDDqLYaeQPpaCEPUkMw1BrKa2xwNS7UQ");
 
 #[program]
 pub mod trading_farm {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
+
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        escrowed_tokens_of_offer_maker_bump: u8,
+        im_offering_this_much: u64,
+        how_much_i_want_of_what_you_have: u64,
+    ) -> ProgramResult {
+        instructions::make_offer::handler(
+            ctx,
+            escrowed_tokens_of_offer_maker_bump,
+            im_offering_this_much,
+            how_much_i_want_of_what_you_have,
+        )
+    }
+
+    pub fn accept_offer(ctx: Context<AcceptOffer>) -> ProgramResult {
+        instructions::accept_offer::handler(ctx)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
