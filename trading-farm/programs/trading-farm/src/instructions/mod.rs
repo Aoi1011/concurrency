e0c92fb214d9c983f4799d6de94b460791e8424d
Aoi <ht.so.ik0102@gmail.com>
2022-01-05 08:07:52 +0700

    handler function

--- trading-farm/programs/trading-farm/src/instructions/mod.rs
+++ trading-farm/programs/trading-farm/src/instructions/mod.rs
@@ -1,7 +1,9 @@
 pub mod make_offer;
 pub mod accept_offer;
 pub mod cancel_offer;
+pub mod airdrop;
 
 pub use make_offer::*;
 pub use accept_offer::*;
 pub use cancel_offer::*;
+pub use airdrop::*;
