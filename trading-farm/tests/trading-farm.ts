18e2e79526107777a66b8ef9a2cc209192224de6
Aoi <ht.so.ik0102@gmail.com>
2022-01-05 20:45:35 +0700

    update test case

--- trading-farm/tests/trading-farm.ts
+++ trading-farm/tests/trading-farm.ts
@@ -1,15 +1,45 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { TradingFarm } from '../target/types/trading_farm';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+// import { TradingFarm } from "../target/types/trading_farm";
+import * as assert from "assert";
+import * as spl from "@solana/spl-token";
+import NodeWallet from "@project-serum/anchor/dist/cjs/provider";
+import { PublicKey } from "@solana/web3.js";
 
-describe('trading-farm', () => {
+const provider = anchor.Provider.env();
+anchor.setProvider(provider);
+const mainProgram = anchor.workspace.TradingFarm;
 
+describe("trading-farm", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  
 
-  const program = anchor.workspace.TradingFarm as Program<TradingFarm>;
+ 
 
-  it('Is initialized!', async () => {
+  let offerMakerPigTokenAccount: anchor.web3.PublicKey;
+  let offerTakerCowTokenAccount: anchor.web3.PublicKey;
+  let offerTakerPigTokenAccount: anchor.web3.PublicKey;
+  let offerMakerCowTokenAccount: anchor.web3.PublicKey;
+
+  let cowMint: spl.Token;
+  let pigMint: spl.Token;
+
+  let offerTaker = anchor.web3.Keypair.generate();
+
+  before(async () => {
+    const wallet = program.provider.wallet as NodeWallet;
+
+    cowMint = await spl.Token.createMint(
+      program.provider.connection,
+      wallet.payer,
+      wallet.publicKey,
+      wallet.publicKey,
+      0,
+      spl.TOKEN_PROGRAM_ID
+    );
+  });
+
+  it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.rpc.initialize({});
     console.log("Your transaction signature", tx);
