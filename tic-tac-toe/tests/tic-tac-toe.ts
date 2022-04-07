7304b217a2f281632104ba31eadd7d3d13730b24
Aoi <ht.so.ik0102@gmail.com>
2022-04-08 06:04:36 +0700

    Add test case

--- tic-tac-toe/tests/tic-tac-toe.ts
+++ tic-tac-toe/tests/tic-tac-toe.ts
@@ -1,8 +1,31 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program } from "@project-serum/anchor";
 import { TicTacToe } from "../target/types/tic_tac_toe";
 
 import chai, { expect } from "chai";
+
+async function play(
+  program,
+  game,
+  player,
+  tile,
+  expectedTurn,
+  expectedGameState,
+  expectedBoard
+) {
+  await program.rpc.play(tile, {
+    accounts: {
+      player: player.publicKey,
+      game,
+    },
+    signers: player instanceof (anchor.Wallet as any) ? [] : [player],
+  });
+
+  const gameState = await program.account.game.fetch(game);
+  expect(gameState.turn).to.equal(expectedTurn);
+  expect(gameState.state).to.eql(expectedGameState);
+  expect(gameState.board).to.eql(expectedBoard);
+}
 
 describe("tic-tac-toe", () => {
   // Configure the client to use the local cluster.
@@ -36,5 +59,73 @@ describe("tic-tac-toe", () => {
       [null, null, null],
       [null, null, null],
     ]);
+  });
+
+  it("player on wins", async () => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = program.provider.wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+    await program.rpc.setupGame(playerTwo.publicKey, {
+      accounts: {
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [gameKeypair],
+    });
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players).to.eql([
+      playerOne.publicKey,
+      playerTwo.publicKey,
+    ]);
+    expect(gameState.state).to.eql({ active: {} });
+    expect(gameState.board).to.eql([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+
+    await play(
+      program,
+      gameKeypair.publicKey,
+      playerOne,
+      { row: 0, column: 0 },
+      2,
+      { active: {} },
+      [
+        [{ x: {} }, null, null],
+        [null, null, null],
+        [null, null, null],
+      ]
+    );
+
+    try {
+      await play(
+        program,
+        gameKeypair.publicKey,
+        playerOne,
+        { row: 1, column: 0 },
+        2,
+        { active: {} },
+        [
+          [{ x: {} }, null, null],
+          [null, null, null],
+          [null, null, null],
+        ]
+      );
+      chai.assert(false, "should've failed but didn't");
+    } catch (error) {
+      expect(error).to.be.instanceOf(AnchorError);
+      const err: AnchorError = error;
+      expect(err.error.errorCode.code).to.equal("NotPlayersTurn");
+      expect(err.error.errorCode.number).to.equal(6003);
+      expect(err.program.equals(program.programId)).is.true;
+      expect(err.error.comparedValues).to.deep.equal([
+        playerTwo.publicKey,
+        playerOne.publicKey,
+      ]);
+    }
   });
 });
