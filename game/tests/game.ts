fb0ad77acf815149ad7ed34d25e76358f73c3b20
Aoi <ht.so.ik0102@gmail.com>
2022-04-10 09:28:08 +0700

    Add test case and test successfully

--- game/tests/game.ts
+++ game/tests/game.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Game } from "../target/types/game";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { expect } from "chai";
 
 describe("game", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +10,38 @@ describe("game", () => {
 
   const program = anchor.workspace.Game as Program<Game>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it("Sets and changes name!", async () => {
+    const [userStatsPDA, _] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("user-stats"),
+        anchor.getProvider().wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const tx = await program.methods
+      .createUserStats("brian")
+      .accounts({
+        user: anchor.getProvider().wallet.publicKey,
+        userStats: userStatsPDA,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).name).to.equal(
+      "brian"
+    );
+
+    await program.methods
+      .changeUserName("tom")
+      .accounts({
+        user: anchor.getProvider().wallet.publicKey,
+        userStats: userStatsPDA,
+      })
+      .rpc();
+
+    expect((await program.account.userStats.fetch(userStatsPDA)).name).to.equal(
+      "tom"
+    );
   });
 });
