6640cd9236093d886b2bfc3be6726200b492587b
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-31 16:53:00 +0700

    Resolve competence

--- main.go
+++ main.go
@@ -10,17 +10,23 @@ func main() {
 	src := []int{1, 2, 3, 4, 5}
 	dst := []int{}
 
+	c := make(chan int)
+
 	for _, s := range src {
-		go func(s int) {
+		go func(s int, c chan int) {
 			result := s * 2
 
-			dst = append(dst, result)
-		}(s)
+			c <- result
+		}(s, c)
 	}
 
-	time.Sleep(time.Second)
-	fmt.Println(dst)
+	for _ = range src {
+		num := <-c
+		dst = append(dst, num)
+	}
 
+	fmt.Println(dst)
+	close(c)
 }
 
 func getLuckyNum(c chan<- int) {
