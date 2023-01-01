bd1f7f4dc27e973e0e735774a948db385ac38cdd
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-01 18:18:52 +0700

    WaitGroup 1

--- main.go
+++ main.go
@@ -8,24 +8,19 @@ import (
 )
 
 func main() {
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello from %d!\n", id)
+	}
+
+	const numGreeters = 5
 	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fmt.Println("1st goroutine sleeping...")
-		time.Sleep(time.Second * 2)
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fmt.Println("2nd goroutine sleeping...")
-		time.Sleep(time.Second * 2)
-	}()
+	wg.Add(numGreeters)
+	for i := 0; i < numGreeters; i++ {
+		go hello(&wg, i+1)
+	}
 
 	wg.Wait()
-	fmt.Println("All goroutine complete.")
 }
 
 func getLuckyNum(c chan<- int) {
