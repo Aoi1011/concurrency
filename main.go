0124f1bac7f816d4ede5696915388bdf2ee58077
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-01 15:03:57 +0700

    Livelock

--- main.go
+++ main.go
@@ -1,36 +1,47 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 func main() {
-	type value struct {
-		mu    sync.Mutex
-		value int
+	cadence := sync.NewCond(&sync.Mutex{})
+
+	go func() {
+		for range time.Tick(1 * time.Millisecond) {
+			cadence.Broadcast()
+		}
+	}()
+
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
 	}
 
-	var wg sync.WaitGroup
-	printSum := func(v1, v2 *value) {
-		defer wg.Done()
-		v1.mu.Lock()
-		defer v1.mu.Unlock()
-
-		time.Sleep(2 * time.Second)
-		v2.mu.Lock()
-		defer v2.mu.Unlock()
-
-		fmt.Printf("sum=%d", v1.value+v2.value)
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool {
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1)
+		takeStep()
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprintf(out, ". Success")
+			return true
+		}
+		takeStep()
+		atomic.AddInt32(dir, -1)
+		return false
 	}
 
-	var a, b value
-	wg.Add(2)
-	go printSum(&a, &b)
-	go printSum(&b, &a)
-	wg.Wait()
+	var left, right int32
+	tryLeft := func(out *bytes.Buffer) bool { return tryDir("left", &left, out) }
+	tryRight := func(out *bytes.Buffer) bool { return tryDir("right", &right, out) }
+
+	fmt.Printf("Left: %t \nRight: %t \n", tryLeft(&bytes.Buffer{}), tryRight(&bytes.Buffer{}))
 }
 
 func getLuckyNum(c chan<- int) {
