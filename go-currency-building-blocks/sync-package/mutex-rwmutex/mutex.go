cb2115affbd267969218ee10084a10264b017d08
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-01 18:45:18 +0700

    Update mutex

--- go-currency-building-blocks/sync-package/mutex-rwmutex/mutex.go
+++ go-currency-building-blocks/sync-package/mutex-rwmutex/mutex.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	var count int
+	var lock sync.Mutex
+
+	increment := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Incrementing: %d\n", count)
+	}
+
+	decrement := func() {
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("Decrementing: %d\n", count)
+	}
+
+	// Increment
+	var arithemetic sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		arithemetic.Add(1)
+		go func() {
+			defer arithemetic.Done()
+			increment()
+		}()
+	}
+
+	// Decrement
+	for i := 0; i <= 5; i++ {
+		arithemetic.Add(1)
+		go func() {
+			defer arithemetic.Done()
+			decrement()
+		}()
+	}
+
+	arithemetic.Wait()
+	fmt.Println("Arithmetic complete.")
 }
